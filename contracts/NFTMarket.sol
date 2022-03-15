c005b03bd4e8f15488941f6b3fb35da19f3e0da2
I-M-F <matimu.f.ignatius@gmail.com>
2022-03-15 16:14:24 +0300

    Updating the home page

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract NFTMArket is ReentrancyGuard {
+contract NFTMarket is ReentrancyGuard {
     using Counters for Counters.Counter;
     Counters.Counter private _itemIds;
     Counters.Counter private _itemsSold;
--- test/nftmarket-test.js
+++ test/nftmarket-test.js
@@ -4,7 +4,7 @@ const { ethers } = require("hardhat");
 describe("NFTMarket", function () {
     it("Should create and Execute Market Sales", async function () {
         /* deploy the marketplace */
-        const Market = await ethers.getContractFactory("NFTMArket")
+        const Market = await ethers.getContractFactory("NFTMarket")
         const market = await Market.deploy()
         await market.deployed()
         const marketAddress = market.address
